e2631c9a53ae36874fb5c14f8a27055291c619a0
kpotapovich <kris.potapovich@gmail.com>
2021-03-14 23:58:16 +0300

    init: a function that swaps the digits of this number.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let result = 0;
+  for (let m = Math.abs(n); m > 0; m = Math.trunc(m / 10)) {
+      result = result * 10 + m % 10;
+  }
+  return result;
 }
